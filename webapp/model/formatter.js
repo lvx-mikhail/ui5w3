2e3793dcd2bff54990c7cbfff382fcf76a969133
Khamitsevich Mikhail <mikhail.khamitsevich@sap.com>
2019-05-24 15:51:27 +0000

    unit 4

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -15,6 +15,27 @@ sap.ui.define([], function () {
 			}
 
 			return parseFloat(sValue).toFixed(2);
+		},
+		
+		/**
+		 * Converts the delivery status value from the data into a
+		 * number used for the colorScheme propery of the InfoLabel
+		 * control.
+		 *
+		 * @public
+		 * @param {string} sValue The value to be formatted
+		 * @returns {int} The formatted delivery status value for the
+		 * InfoLabel colorScheme
+		 */
+		deliveryStatus: function(sValue) {
+			switch(sValue) {
+				case "New": return 5;
+				case "In Progress": return 1;
+				case "Canceled": return 3;
+				case "Closed": return 7;
+				default: return 7;
+			}
 		}
+
 	};
 });
